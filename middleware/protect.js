86f8146d0352bc65420ad6aa4f749fe084cb9cbb
Herrero <hemosq@bp1.ad.bp.com>
2017-08-14 10:40:36 +0100

    added logs and updated npm package

--- middleware/protect.js
+++ middleware/protect.js
@@ -3,20 +3,26 @@ var fh = require("fh-mbaas-api");
 module.exports = function (auth, spec) {
 
     return function protect (request, response, next) {
+        var params = {
+            token: request.body.token
+        }
+        if (spec != undefined) {
+            if (spec.groups) {
+                params.groups = spec.groups
+            }
+        }
         fh.service({
             "guid": auth.SAMLAuthProjId,
             "path": "/session/valid",
             "method": "POST",
-            "params": {
-                token: request.body.token
-            }
+            "params": params
         }, function(err, body, service_res) {
-            console.log('call', body, service_res);
             if (err) {
                 // An error occurred
                 console.log('Service call failed: ', err, service_res);
                 return err;
             } else {
+                console.log('call', body, service_res);
                 return next();
             }
         });
